919cfb68e47d23bf6ed650513d69a3932b7938ea
dylanclarke890 <dylanclarke890@gmail.com>
2022-09-06 13:30:56 +0100

    add head/tail methods

--- Queue.js
+++ Queue.js
@@ -1,16 +1,26 @@
 class Queue {
-  get size() {return this.arr.length};
-  
+  get size() {
+    return this.arr.length;
+  }
+
   constructor(/** @type {any[]} */ arr) {
     this.arr = arr || [];
   }
 
+  head() {
+    return this.arr[0];
+  }
+
   enqueue(val) {
-    this.arr.push(val)
+    this.arr.push(val);
   }
 
   dequeue() {
     if (this.arr.length === 0) return null;
     return this.arr.splice(0, 1)[0];
+  }
+
+  tail() {
+    return this.arr[this.arr.length - 1];
   }
 }
