f332202db41bc5d519030ae121c013717b92fb0b
dylanclarke890 <dylanclarke890@gmail.com>
2022-09-06 13:29:13 +0100

    added basic queue class

---
+++ Queue.js
@@ -0,0 +1,16 @@
+class Queue {
+  get size() {return this.arr.length};
+  
+  constructor(/** @type {any[]} */ arr) {
+    this.arr = arr || [];
+  }
+
+  enqueue(val) {
+    this.arr.push(val)
+  }
+
+  dequeue() {
+    if (this.arr.length === 0) return null;
+    return this.arr.splice(0, 1)[0];
+  }
+}
