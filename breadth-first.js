f6e9477ffb69a076d3414371a2fdd668d8682332
dylanclarke890 <dylanclarke890@gmail.com>
2022-09-09 01:37:21 +0100

    tidying up breadth first

--- breadth-first.js
+++ breadth-first.js
@@ -18,55 +18,18 @@ PF.Algorithms.breadthFirst = function (startCoordinates, grid) {
   // Loop through the grid searching for the goal
   while (q.size > 0) {
     const currentLocation = q.dequeue();
-
-    // Explore North
-    const north = exploreInDirection(currentLocation, "North", grid);
-    if (north.status === "Goal") return north.path;
-    else if (north.status === "Valid") {
-      q.enqueue(north);
-      paths.enqueue(new PF.utils.Path(north.x, north.y));
-    }
-
-    // Explore East
-    const east = exploreInDirection(currentLocation, "East", grid);
-    if (east.status === "Goal") return east.path;
-    else if (east.status === "Valid") {
-      q.enqueue(east);
-      paths.enqueue(new PF.utils.Path(east.x, east.y));
-    }
-
-    // Explore South
-    const south = exploreInDirection(currentLocation, "South", grid);
-    if (south.status === "Goal") return south.path;
-    else if (south.status === "Valid") {
-      q.enqueue(south);
-      paths.enqueue(new PF.utils.Path(south.x, south.y));
-    }
-
-    // Explore West
-    const west = exploreInDirection(currentLocation, "West", grid);
-    if (west.status === "Goal") return west.path;
-    else if (west.status === "Valid") {
-      q.enqueue(west);
-      paths.enqueue(new PF.utils.Path(west.x, west.y));
-    }
+    ["North", "East", "South", "West"].forEach((dir) => {
+      const explored = exploreInDirection(currentLocation, dir, grid);
+      if (explored.status === "Goal") return explored.path;
+      else if (explored.status === "Valid") {
+        q.enqueue(explored);
+        paths.enqueue(new PF.utils.Path(explored.x, explored.y));
+      }
+    });
   }
   // No valid path found
   return false;
 };
-
-// Will check a location's status (a location is "valid" if it is on the grid, is not an "obstacle",
-// and has not yet been visited by our algorithm)
-// Returns "Valid", "Invalid", "Blocked", or "Goal"
-function locationStatus(location, grid) {
-  const { x, y } = location;
-  // location is not on the grid--return false
-  if (x < 0 || x >= gridSize.w || y < 0 || y >= gridSize.h) return "Invalid";
-  else if (grid[y][x] === "Goal") return "Goal";
-  // location is either an obstacle or has been visited
-  else if (grid[y][x] !== "Empty") return "Blocked";
-  else return "Valid";
-}
 
 // Explores the grid from the given location in the given direction
 function exploreInDirection(currentLocation, direction, grid) {
@@ -74,14 +37,18 @@ function exploreInDirection(currentLocation, direction, grid) {
   newPath.push(direction);
 
   let { x, y } = PF.utils.moveInDirection(currentLocation, direction);
-
   const next = {
     x,
     y,
     path: newPath,
     status: "Unknown",
   };
-  next.status = locationStatus(next, grid);
+
+  if (x < 0 || x >= gridSize.w || y < 0 || y >= gridSize.h)
+    next.status = "Invalid";
+  else if (grid[y][x] === "Goal") next.status = "Goal";
+  else if (grid[y][x] !== "Empty") next.status = "Blocked";
+  else next.status = "Valid";
 
   // If this new location is valid, mark it as 'Visited'
   if (next.status === "Valid") grid[next.y][next.x] = "Visited";
