4a6d5f9c0f334c6c85b6bfda197538623e3ed194
dylanclarke890 <dylanclarke890@gmail.com>
2022-09-09 12:06:58 +0100

    added demo file which can use either of the new algorithms

--- demo.js
+++ demo.js
@@ -1,9 +1,40 @@
 const [canvas, ctx] = PF.utils.new2dCanvas("play-area", 680, 560);
-const grid = new PF.Grid(10, 10);
+const matrix = Array.from({ length: 15 }, () =>
+  Array.from({ length: 15 }, () => 0)
+);
+const grid = new PF.Grid(matrix);
 const finder = new PF.Algorithms.BreadthFirst();
-const result = finder.findPath(5, 5, 9, 9, grid);
 
-function update() {}
+const cellSize = 40;
+const sx = 5,
+  sy = 5;
+const result = finder.findPath(sx, sy, 9, 9, grid);
+
+function drawGrid() {
+  for (let i = 0; i < matrix.length; i++)
+    for (let j = 0; j < matrix[i].length; j++) {
+      ctx.fillStyle = matrix[j][i] ? "blue" : "white";
+      ctx.strokeStyle = "grey";
+      ctx.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
+      ctx.strokeRect(j * cellSize, i * cellSize, cellSize, cellSize);
+    }
+}
+
+function drawPath() {
+  for (let i = 0; i < result.length; i++) {
+    const pos = result[i];
+    ctx.fillStyle = "yellow";
+    ctx.strokeStyle = "grey";
+    ctx.fillRect(pos[1] * cellSize, pos[0] * cellSize, cellSize, cellSize);
+    ctx.strokeRect(pos[1] * cellSize, pos[0] * cellSize, cellSize, cellSize);
+  }
+}
+
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawGrid();
+  drawPath();
+}
 
 let stop = false,
   now,
