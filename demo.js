ae36ba649d2d713ef038cb2449e72c96d91aecaa
dylanclarke890 <dylanclarke890@gmail.com>
2022-09-10 01:11:09 +0100

    removed debugger

--- demo.js
+++ demo.js
@@ -16,7 +16,6 @@ const size = PF.settings.squareSize;
 const sx = 0,
   sy = 0;
 const finder = new PF.Algorithms.IDAStar();
-debugger;
 const result = finder.findPath(sx, sy, 14, 14, grid);
 
 const [canvas, ctx] = PF.utils.new2dCanvas("play-area", 600, 600);
