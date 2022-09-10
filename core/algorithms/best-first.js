2017ce3d478c654813934b4e2951c06cb4f39088
dylanclarke890 <dylanclarke890@gmail.com>
2022-09-10 06:45:20 +0100

    added best first

---
+++ core/algorithms/best-first.js
@@ -0,0 +1,15 @@
+/**
+ * Best-First path-finder.
+ * @constructor
+ * @extends PF.Algorithms.AStar
+ * @param {Object} opt
+ * @param {PF.DiagonalMovement} opt.diagonalMovement Allowed diagonal movement. Defaults to "Never".
+ * @param {function} opt.heuristic Heuristic function to estimate the distance
+ */
+PF.Algorithms.BestFirst = class extends PF.Algorithms.AStar {
+  constructor(opt) {
+    super(opt);
+    const orig = this.heuristic;
+    this.heuristic = (dx, dy) => orig(dx, dy) * 1000000;
+  }
+};
