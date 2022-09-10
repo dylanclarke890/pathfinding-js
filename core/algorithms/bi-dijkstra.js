25e6d4dcc6451bb978c2090ce6426dd6833a64ae
dylanclarke890 <dylanclarke890@gmail.com>
2022-09-10 10:41:10 +0100

    added bi dijkstra ago

---
+++ core/algorithms/bi-dijkstra.js
@@ -0,0 +1,16 @@
+var PF = PF || {};
+PF.Algorithms = PF.Algorithms || {};
+
+/**
+ * Dijkstra path-finder.
+ * @constructor
+ * @extends PF.Algorithms.BiAStar
+ * @param {Object} opt
+ * @param {PF.DiagonalMovement} opt.diagonalMovement Allowed diagonal movement. Defaults to "Never".
+ */
+PF.Algorithms.BiDijkstra = class extends PF.Algorithms.BiAStar {
+  constructor(opt) {
+    super(opt);
+    this.heuristic = () => 0;
+  }
+};
